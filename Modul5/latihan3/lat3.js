410dc799262f32250e50d1d8efde773bc5926bf9
fandiiardy@gmail.com <fandiiardy@gmail.com>
2025-10-13 20:00:39 +0800

    add modul 5

---
+++ Modul5/latihan3/lat3.js
@@ -0,0 +1,12 @@
+var panjang = 10;
+var lebar = 5;
+var linebreak = "<br>";
+
+var luas = panjang * lebar;
+
+document.write("<h3>Menghitung Luas Persegi Panjang</h3>");
+document.write("Panjang = " + panjang);
+document.write(linebreak);
+document.write("Lebar = " + lebar);
+document.write(linebreak);
+document.write("<b>Luas Persegi Panjang = " + luas + "</b>");
---
+++ Modul5/latihan4/lat4.js
@@ -0,0 +1,4 @@
+var nama = "Andi";
+var umur = 20;
+
+document.write("Nama saya " + nama + ", umur saya " + umur + " tahun.");
